4f7dad4828cf406d4e8764318c69eda453b7e0df
unknown <rlacjs1021@naver.com>
2025-05-01 03:21:49 +0900

    tailwind

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,11 +1,26 @@
-import { createRoot } from 'react-dom/client';
 import './tailwind.css'; // Tailwind CSS 임포트 (새로 추가)
 import './App.css'; // 기존 스타일 유지
 import App from './App';
 
-const container = document.getElementById('root') as HTMLElement;
-const root = createRoot(container);
-root.render(<App />);
+// const container = document.getElementById('root') as HTMLElement;
+// const root = createRoot(container);
+// root.render(<App />);
+
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "./common/components/ui/toaster";
+import { TooltipProvider } from "./common/components/ui/tooltip";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
+    <Toaster />
+  </StrictMode>,
+);
+
 
 // calling IPC exposed from preload script
 window.electron.ipcRenderer.once('ipc-example', (arg) => {
